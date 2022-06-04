3957561052dee79c00a5574798f2dc8f36909ee1
ttbinh139 <ttbinh139@gmail.com>
2022-06-04 23:35:37 +0000

    Double check database connection, define routing for users, listing and messages

---
+++ routes/listings.js
@@ -0,0 +1,27 @@
+/*
+ * All routes for Listings are defined here
+ * Since this file is loaded in server.js into api/listings,
+ *   these routes are mounted onto /widgets
+ * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
+ */
+
+const express = require('express');
+const router  = express.Router();
+
+module.exports = (db) => {
+  router.get("/", (req, res) => {
+    let query = `SELECT * FROM listings`;
+    console.log(query);
+    db.query(query)
+      .then(data => {
+        const listings = data.rows;
+        res.json({ listings });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+  return router;
+};
--- server.js
+++ server.js
@@ -33,16 +33,29 @@ app.use(
 
 app.use(express.static("public"));
 
-// Separated Routes for each Resource
-// Note: Feel free to replace the example routes below with your own
+/**
+ * Define routing
+ */
 const usersRoutes = require("./routes/users");
-const widgetsRoutes = require("./routes/widgets");
+const listingsRoutes = require("./routes/listings");
+const messagesRoutes = require("./routes/messages");
+//const widgetsRoutes = require("./routes/widgets");
 
-// Mount all resource routes
-// Note: Feel free to replace the example routes below with your own
+
+/**
+ * ROUTING for API
+ */
 app.use("/api/users", usersRoutes(db));
-app.use("/api/widgets", widgetsRoutes(db));
-// Note: mount other resources here, using the same pattern above
+app.use("/api/listings", listingsRoutes(db));
+app.use("/api/messages", messagesRoutes(db));
+
+/**
+ * ROUTING for normal requests
+ */
+app.use("/users", usersRoutes(db));
+app.use("/listings", listingsRoutes(db));
+app.use("/messages", messagesRoutes(db));
+
 
 // Home page
 // Warning: avoid creating more routes in this file!
