4aebba387c39f5ac9820806c78abf27b75b6e54e
Sang Lee <melonaster@gmail.com>
2022-06-04 06:04:22 +0000

    added message seeds

---
+++ db/seeds/06_message.sql
@@ -0,0 +1,17 @@
+-- message table seeds here (Example)
+
+INSERT INTO message (id, receiver_id, sender_id,listing_id)
+VALUES (1, 1, 6, 1);
+
+INSERT INTO message (id, receiver_id, sender_id,listing_id)
+VALUES (2, 2, 7, 2);
+
+INSERT INTO message (id, receiver_id, sender_id,listing_id)
+VALUES (3, 3, 8, 3);
+
+INSERT INTO message (id, receiver_id, sender_id,listing_id)
+VALUES (4, 4, 9, 4);
+
+INSERT INTO message (id, receiver_id, sender_id,listing_id)
+VALUES (5, 5, 10, 5);
+
