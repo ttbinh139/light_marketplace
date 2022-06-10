c41bb76c0b1adb936bef4e139555cd2013cacfaa
vagrant <melonaster@gmail.com>
2022-06-10 04:13:35 +0000

    Commiting

--- routes/newad.js
+++ routes/newad.js
@@ -44,7 +44,7 @@ module.exports = (db) => {
     ])
       .then((values) => {
         let listing_id = values[1];
-        sellerHelper.addNewFavorites(listing_id, req.session.userId["id"], db);
+        sellerHelper.addNewFavourites(listing_id, req.session.userId["id"], db);
         sellerHelper.addNewPhotos(listing_id, files, db);
       })
       .then((result) => {
